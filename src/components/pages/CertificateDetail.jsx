5d8a9bf80c9ad48471a440f25cc608058726a536
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-08-27 02:27:38 +0700

    add Fetch Data LocalStorage

---
+++ src/components/pages/CertificateDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CertificateDetail = () => {
+  return (
+    <div>CertificateDetail</div>
+  )
+}
+
+export default CertificateDetail
--- src/components/pages/GetCertificates.jsx
+++ src/components/pages/GetCertificates.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   Typography,
@@ -11,17 +11,18 @@ import {
   DialogHeader,
   DialogBody,
   DialogFooter,
+  Spinner,
 } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 
 const TABLE_ROWS = [
   {
     name: "TOEIC Certificates - 2012",
-    address: "0xAbCdEf0123456789AbCdEf0123456789AbCdEf01",
+    address: "0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c",
   },
   {
     name: "UIT Graduation Certificate",
-    address: "0xDeadBeefDeadBeefDeadBeefDeadBeefDeadBeef",
+    address: "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
   },
   {
     name: "Coursera - UI/UX Design MasterTrack Certificate",
@@ -29,7 +30,7 @@ const TABLE_ROWS = [
   },
   {
     name: "Writing Tools & Hacks Course - Udemy Certificate of Completion",
-    address: "0x1234567890ABCDEF1234567890ABCDEF12345678",
+    address: "0x617F2E2fD72FD9D5503197092aC168c91465E7f2",
   },
   {
     name: "Cisco Certified Network Associate",
@@ -41,6 +42,18 @@ function GetCertificates() {
   const [tableRows, setTableRows] = useState(TABLE_ROWS);
   const [showModal, setShowModal] = useState(false);
   const [curIndex, setCurIndex] = useState(-1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [dataFetched, setDataFetched] = useState(false);
+  const [refreshKey, setRefreshKey] = useState(0); // Thêm state key duy nhất
+  const gasFee = 0.0001 + Math.random() * (0.001 - 0.0001);
+
+  useEffect(() => {
+    const fetchedData = localStorage.getItem("fetchedData");
+    if (fetchedData) {
+      setDataFetched(true);
+      setTableRows(JSON.parse(fetchedData));
+    }
+  }, []);
 
   const removeRow = (index) => {
     const newTableRows = tableRows.filter((_, i) => i !== index);
@@ -59,97 +72,125 @@ function GetCertificates() {
           Fetch all your certificates stored in blockchain and spend amount of
           gas.
         </Typography>
-        <Button color="green" className="w-fit self-center mt-5">
-          Fetch data
-        </Button>
+        <div className="flex justify-center items-center mt-5 gap-3">
+          <Button
+            color="green"
+            className="w-fit self-center"
+            onClick={() => {
+              setIsLoading(true);
+              setTimeout(() => {
+                setDataFetched(true);
+                setIsLoading(false);
+                localStorage.setItem("fetchedData", JSON.stringify(tableRows));
+                setRefreshKey(refreshKey + 1);
+              }, 3000);
+            }}
+          >
+            {isLoading ? <Spinner className="h-4 w-4" /> : "Fetch data"}
+          </Button>
+          <Typography color="green" variant="h6">
+            Gas fee: {gasFee} ETH.
+          </Typography>
+        </div>
       </div>
       <div className="flex justify-center">
-        <Card className="h-full w-[80%] mt-10 overflow-hidden">
+        <Card
+          key={refreshKey}
+          className="h-full w-[80%] mt-10 overflow-hidden animate__animated animate__fadeInUp"
+        >
           <table className="w-full min-w-max table-auto text-left">
             <tbody>
-              {tableRows.map(({ name, address }, index) => {
-                const isLast = index === TABLE_ROWS.length - 1;
-                const classes =
-                  (isLast ? "p-4" : "p-4 border-b border-blue-gray-50") +
-                  " flex items-center hover:bg-gray-200 cursor-pointer px-5";
+              {dataFetched ? (
+                tableRows.map(({ name, address }, index) => {
+                  const isLast = index === TABLE_ROWS.length - 1;
+                  const classes =
+                    (isLast ? "p-4" : "p-4 border-b border-blue-gray-50") +
+                    ` flex items-center hover:bg-gray-200 cursor-pointer px-5`;
 
-                return (
-                  <tr key={index}>
-                    <td className={classes}>
-                      <div className="mr-2">
-                        <i className="fas fa-file-pdf text-red-500 mr-1"></i>
-                      </div>
-                      <div className="flex flex-col gap-1">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal pl-2"
-                        >
-                          {name}
-                        </Typography>
-                        <Typography className="font-normal text-gray-500 text-xs pl-2">
-                          {address}
-                        </Typography>
-                      </div>
-                      <div className="ml-auto">
-                        <Menu>
-                          <MenuHandler>
-                            <i className="fas fa-ellipsis-vertical text-gray-400 p-1"></i>
-                          </MenuHandler>
-                          <MenuList>
-                            <MenuItem className="flex flex-row">
-                              <Link
-                                to={`/share/${address}`}
+                  return (
+                    <tr key={index}>
+                      <td className={classes}>
+                        <div className="mr-2">
+                          <i className="fas fa-file-pdf text-red-500 mr-1"></i>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal pl-2"
+                          >
+                            {name}
+                          </Typography>
+                          <Typography className="font-normal text-gray-500 text-xs pl-2">
+                            {address}
+                          </Typography>
+                        </div>
+                        <div className="ml-auto">
+                          <Menu>
+                            <MenuHandler>
+                              <i className="fas fa-ellipsis-vertical text-gray-400 p-1"></i>
+                            </MenuHandler>
+                            <MenuList>
+                              <MenuItem className="flex flex-row">
+                                <Link
+                                  to={`/share/${address}`}
+                                  className="flex flex-row"
+                                >
+                                  <div className="my-auto">
+                                    <i className="fas fa-share text-blue-gray-500 mr-1"></i>
+                                  </div>
+                                  <Typography
+                                    variant="small"
+                                    color="blue-gray-500"
+                                    className="font-normal pl-2"
+                                  >
+                                    Share
+                                  </Typography>
+                                </Link>
+                              </MenuItem>
+                              <MenuItem
                                 className="flex flex-row"
+                                onClick={() => {
+                                  setCurIndex(index);
+                                  setShowModal(true);
+                                }}
                               >
                                 <div className="my-auto">
-                                  <i className="fas fa-share text-blue-gray-500 mr-1"></i>
+                                  <i className="fas fa-trash text-blue-gray-500 mr-1"></i>
                                 </div>
                                 <Typography
                                   variant="small"
-                                  color="blue-gray-500"
                                   className="font-normal pl-2"
                                 >
-                                  Share
+                                  Remove
                                 </Typography>
-                              </Link>
-                            </MenuItem>
-                            <MenuItem
-                              className="flex flex-row"
-                              onClick={() => {
-                                setCurIndex(index);
-                                setShowModal(true);
-                              }}
-                            >
-                              <div className="my-auto">
-                                <i className="fas fa-trash text-blue-gray-500 mr-1"></i>
-                              </div>
-                              <Typography
-                                variant="small"
-                                className="font-normal pl-2"
-                              >
-                                Remove
-                              </Typography>
-                            </MenuItem>
-                            <MenuItem className="flex flex-row">
-                              <div className="my-auto">
-                                <i className="fas fa-rotate-left text-red-500 mr-1"></i>
-                              </div>
-                              <Typography
-                                variant="small"
-                                color="red"
-                                className="font-normal pl-2"
-                              >
-                                Revoke
-                              </Typography>
-                            </MenuItem>
-                          </MenuList>
-                        </Menu>
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
+                              </MenuItem>
+                              <MenuItem className="flex flex-row">
+                                <div className="my-auto">
+                                  <i className="fas fa-rotate-left text-red-500 mr-1"></i>
+                                </div>
+                                <Typography
+                                  variant="small"
+                                  color="red"
+                                  className="font-normal pl-2"
+                                >
+                                  Revoke
+                                </Typography>
+                              </MenuItem>
+                            </MenuList>
+                          </Menu>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr>
+                  <td className="p-4">
+                    <Typography>No certificates fetched</Typography>
+                  </td>
+                </tr>
+              )}
             </tbody>
           </table>
         </Card>
